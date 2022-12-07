6a036827bb0d678c29e02202a68d8c707d775bf2
Nadine Safwat <nadinhassann@yahoo.com>
2022-12-07 22:04:49 +0000

    .

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,19 +12,19 @@ def roman_to_int(roman_string):
         return (0)
     if not isinstance(roman_string, str):
         return (0)
-    numdict = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
-    keyslst = list(numdict.keys())
+    ndic = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
+    keyslst = list(ndic.keys())
     n = 0
     romnum = 0
     numlst = [0]
     for char in roman_string:
         for num in keyslst:
             if num == char:
-                if numdict.get(char) <= romnum:
+                if ndic.get(char) <= romnum:
                     n = n + sub(numlst)
-                    numlst = [numdict.get(char)]
+                    numlst = [ndic.get(char)]
                 else:
-                    numlst.append(numdict.get(char))
-                romnum = numdict.get(char)
+                    numlst.append(ndic.get(char))
+                romnum = ndic.get(char)
     n = n + sub(numlst)
     return (n)
