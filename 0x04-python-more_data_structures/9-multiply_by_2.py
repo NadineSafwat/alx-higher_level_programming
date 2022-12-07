71be97a6063ed21c5b4d90010664e920c7402302
Nadine Safwat <nadinhassann@yahoo.com>
2022-12-07 19:53:02 +0000

    fixing bug + task number 10

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     dictionary_cpy = a_dictionary.copy()
-    keyslst = list(dictionary_cpy.key())
+    keyslst = list(dictionary_cpy.keys())
     for key in keyslst:
         dictionary_cpy[key] *= 2
     return (dictionary_cpy)
