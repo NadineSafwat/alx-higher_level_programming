83a2da2d6fbca4e4c3407c601b83622e0c108712
Nadine Safwat <nadinhassann@yahoo.com>
2022-12-07 18:46:49 +0000

    fixing second task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    nlst = list(map(lambda n: replace if n == search else n, mylist))
+    nlst = list(map(lambda n: replace if n == search else n, my_list))
 
     return (nlst)
