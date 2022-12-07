21fd31529949c35b8844213c8a6e3804c9e88974
Nadine Safwat <nadinhassann@yahoo.com>
2022-12-07 22:09:10 +0000

    .

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,9 +9,9 @@ def sub(numlst):
 
 def roman_to_int(roman_string):
     if not roman_string:
-        return (0)
+        return 0
     if not isinstance(roman_string, str):
-        return (0)
+        return 0
     ndic = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
     keyslst = list(ndic.keys())
     n = 0
