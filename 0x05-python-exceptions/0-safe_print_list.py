2ef421ba4dc7e7c8d0fb921bfbd2c20871dfec6a
Nadine Safwat <nadinhassann@yahoo.com>
2022-12-19 14:14:36 +0000

    .

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     n = 0
 
@@ -9,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         except:
             break
         else:
-            n = n + 1
+            n += 1
 
     print()
     return (n)
