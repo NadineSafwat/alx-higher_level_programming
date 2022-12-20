329eacd85ee4fecbcba2cb7577c01d0acecec977
Nadine Safwat <nadinhassann@yahoo.com>
2022-12-20 12:10:16 +0000

    second task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define class Square."""
+
+
+class Square:
+    """Represents square."""
+
+    def __init__(self, size):
+        """Initializing a new Square.
+        Args:
+            size (int): The size of new square.
+        """
+        self.__size = size
