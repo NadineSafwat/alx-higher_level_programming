56e19ab49a3a532bc41c16d5264860924784672c
nadinesafwat <nadinhassann@yahoo.com>
2024-01-10 16:50:58 +0200

    fixing pycodestyle errors in script no. 11 and 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,12 +7,13 @@ def sub(numlst):
             subtract = subtract + i
     return (lstmax - subtract)
 
+
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
     if not isinstance(roman_string, str):
         return 0
-    ndic = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
+    ndic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     keyslst = list(ndic.keys())
     n = 0
     romnum = 0
